aad6c4215a9c5854cf8ceb6a452a210667622b6d
S V Kaushik <83902056+Kaushik2210@users.noreply.github.com>
2026-07-31 08:37:18 +0530

    day 13: add constructor and multi-level abstract subclass example

--- src/day13/AbstractionDemo.java
+++ src/day13/AbstractionDemo.java
@@ -6,14 +6,24 @@ public class AbstractionDemo {
 
         Shape square = new Square(3.0);
         square.describe();
+
+        Shape triangle = new Triangle(4.0, 5.0);
+        triangle.describe();
+        System.out.println(triangle.name + " has " + ((Triangle) triangle).sides() + " sides");
     }
 }
 
 abstract class Shape {
+    String name; // shared field
+
+    Shape(String name) { // constructor -- only ever called via super(...)
+        this.name = name;
+    }
+
     abstract double area(); // no body -- subclasses must implement this
 
     void describe() { // concrete method, shared by all shapes
-        System.out.println("This shape has area " + area());
+        System.out.println(name + " has area " + area());
     }
 }
 
@@ -21,6 +31,7 @@ class Circle extends Shape {
     double radius;
 
     Circle(double radius) {
+        super("Circle");
         this.radius = radius;
     }
 
@@ -34,11 +45,41 @@ class Square extends Shape {
     double side;
 
     Square(double side) {
+        super("Square");
         this.side = side;
     }
 
     @Override
     double area() {
         return side * side;
+    }
+}
+
+// still abstract: doesn't implement area(), only adds a new abstract method
+abstract class Polygon extends Shape {
+    Polygon(String name) {
+        super(name);
+    }
+
+    abstract int sides();
+}
+
+class Triangle extends Polygon {
+    double base, height;
+
+    Triangle(double base, double height) {
+        super("Triangle");
+        this.base = base;
+        this.height = height;
+    }
+
+    @Override
+    double area() {
+        return 0.5 * base * height;
+    }
+
+    @Override
+    int sides() {
+        return 3;
     }
 }
