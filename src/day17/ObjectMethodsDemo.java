aae88f022053877a1c26d68824c8a982e6e0d8f8
Kaushik <svkaushik2210@gmail.com>
2026-08-04 08:49:28 +0530

    day 17: add equals/hashCode override and HashSet example

--- src/day17/ObjectMethodsDemo.java
+++ src/day17/ObjectMethodsDemo.java
@@ -1,3 +1,7 @@
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
 public class ObjectMethodsDemo {
 
     public static void main(String[] args) {
@@ -6,6 +10,14 @@ public class ObjectMethodsDemo {
 
         Raw r = new Raw();
         System.out.println(r); // default Object.toString() -- ClassName@hashcode
+
+        Person a = new Person("Asha", 30);
+        Person b = new Person("Asha", 30);
+        System.out.println("a.equals(b) = " + a.equals(b)); // true -- logical equality, not reference
+
+        Set<Person> people = new HashSet<>();
+        people.add(a);
+        System.out.println("people.contains(b) = " + people.contains(b)); // true because hashCode() is consistent too
     }
 }
 
@@ -21,6 +33,18 @@ class Person {
     @Override
     public String toString() {
         return "Person{name=" + name + ", age=" + age + "}";
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (!(obj instanceof Person other)) return false;
+        return age == other.age && name.equals(other.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
     }
 }
 
