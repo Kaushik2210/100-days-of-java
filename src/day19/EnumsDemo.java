75d0bd0377226c98a9797ffa702b80e03aa60d29
Kaushik <svkaushik2210@gmail.com>
2026-08-06 13:22:58 +0530

    day 19: add enum with fields and constructor example

--- src/day19/EnumsDemo.java
+++ src/day19/EnumsDemo.java
@@ -16,9 +16,30 @@ public class EnumsDemo {
             default -> description = "Weekday";
         }
         System.out.println(today + " is a " + description);
+
+        System.out.println("Earth surface gravity = " + Planet.EARTH.surfaceGravity());
     }
 }
 
 enum Day {
     MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY
+}
+
+enum Planet {
+    MERCURY(3.303e+23, 2.4397e6),
+    VENUS(4.869e+24, 6.0518e6),
+    EARTH(5.976e+24, 6.37814e6);
+
+    private final double mass;   // kilograms
+    private final double radius; // meters
+
+    Planet(double mass, double radius) {
+        this.mass = mass;
+        this.radius = radius;
+    }
+
+    double surfaceGravity() {
+        final double G = 6.67300E-11;
+        return G * mass / (radius * radius);
+    }
 }
