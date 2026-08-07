d14c96d65202c1a547c0265454ea9c1d16475044
Kaushik <svkaushik2210@gmail.com>
2026-08-07 09:52:22 +0530

    day 20: add inner class example

--- src/day20/NestedClassesDemo.java
+++ src/day20/NestedClassesDemo.java
@@ -3,11 +3,17 @@ public class NestedClassesDemo {
     public static void main(String[] args) {
         Computer.Battery battery = new Computer.Battery(5000); // no Computer instance needed
         System.out.println("battery capacity = " + battery.capacityMah + " mAh");
+
+        Computer laptop = new Computer("ThinkPad");
+        laptop.fanSpeed = 2200;
+        Computer.DiagnosticsReport report = laptop.new DiagnosticsReport(); // built through the outer instance
+        System.out.println(report.summarize());
     }
 }
 
 class Computer {
     String model;
+    int fanSpeed;
 
     Computer(String model) {
         this.model = model;
@@ -18,6 +24,12 @@ class Computer {
 
         Battery(int capacityMah) {
             this.capacityMah = capacityMah;
+        }
+    }
+
+    class DiagnosticsReport { // tied to one specific Computer instance
+        String summarize() {
+            return model + " running fan at " + fanSpeed + " RPM";
         }
     }
 }
