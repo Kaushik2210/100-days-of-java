c2a3f6f3c51136ed91e2cb9048eedd4f88fd68db
Kaushik <svkaushik2210@gmail.com>
2026-08-11 08:28:14 +0530

    day 24: add LinkedHashSet and TreeSet examples

--- src/day24/SetsDemo.java
+++ src/day24/SetsDemo.java
@@ -1,5 +1,7 @@
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class SetsDemo {
 
@@ -16,5 +18,23 @@ public class SetsDemo {
         letters.add("a");
         letters.add("b");
         System.out.println(letters); // order is unspecified -- don't rely on it
+
+        Set<String> insertionOrdered = new LinkedHashSet<>();
+        insertionOrdered.add("c");
+        insertionOrdered.add("a");
+        insertionOrdered.add("b");
+        System.out.println(insertionOrdered); // [c, a, b] -- matches insertion order
+
+        Set<Integer> scores = new TreeSet<>();
+        scores.add(85);
+        scores.add(42);
+        scores.add(67);
+        System.out.println(scores); // [42, 67, 85] -- always sorted
+
+        Set<String> byLength = new TreeSet<>((a, b) -> a.length() - b.length());
+        byLength.add("kiwi");
+        byLength.add("fig");
+        byLength.add("apple");
+        System.out.println(byLength); // sorted by string length
     }
 }
