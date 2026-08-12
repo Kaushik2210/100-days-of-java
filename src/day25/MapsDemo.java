245a3a164a0438d126d08d0be540266e08473f28
Kaushik <svkaushik2210@gmail.com>
2026-08-12 12:12:57 +0530

    day 25: add TreeMap and merge/word-count example

--- src/day25/MapsDemo.java
+++ src/day25/MapsDemo.java
@@ -1,5 +1,6 @@
 import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class MapsDemo {
 
@@ -16,5 +17,17 @@ public class MapsDemo {
         for (Map.Entry<String, Integer> entry : ages.entrySet()) {
             System.out.println(entry.getKey() + " -> " + entry.getValue());
         }
+
+        Map<String, Integer> sortedByKey = new TreeMap<>();
+        sortedByKey.put("banana", 3);
+        sortedByKey.put("apple", 5);
+        sortedByKey.put("cherry", 1);
+        System.out.println(sortedByKey); // always sorted by key
+
+        Map<String, Integer> wordCounts = new HashMap<>();
+        for (String word : new String[]{"a", "b", "a", "c", "a"}) {
+            wordCounts.merge(word, 1, Integer::sum); // increments the count, starting from 0
+        }
+        System.out.println(wordCounts);
     }
 }
