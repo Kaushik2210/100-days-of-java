774dfd220af53b1cf8742860f22fbc66b70e0244
Kaushik <svkaushik2210@gmail.com>
2026-08-13 20:32:38 +0530

    day 26: add Comparable and Comparator sorting example

--- src/day26/IteratorsAndOrderingDemo.java
+++ src/day26/IteratorsAndOrderingDemo.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Iterator;
 import java.util.List;
 
@@ -19,5 +21,43 @@ public class IteratorsAndOrderingDemo {
             }
         }
         System.out.println(numbers); // [1, 3, 5]
+
+        List<Person> people = new ArrayList<>(List.of(
+            new Person("Kiran", 25),
+            new Person("Asha", 30),
+            new Person("Ravi", 25)));
+
+        Collections.sort(people); // uses Person.compareTo() -- natural order: age ascending
+        System.out.println(people);
+
+        Comparator<Person> byName = (a, b) -> a.name.compareTo(b.name);
+        people.sort(byName);
+        System.out.println(people);
+
+        Comparator<Person> byAgeThenName = Comparator
+            .comparingInt((Person p) -> p.age)
+            .thenComparing(p -> p.name);
+        people.sort(byAgeThenName.reversed());
+        System.out.println(people);
+    }
+}
+
+class Person implements Comparable<Person> {
+    String name;
+    int age;
+
+    Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public int compareTo(Person other) {
+        return Integer.compare(this.age, other.age); // natural order: youngest first
+    }
+
+    @Override
+    public String toString() {
+        return name + "(" + age + ")";
     }
 }
