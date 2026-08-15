aa7b4bf7dc759d28c4c414703c332011e1e725f7
Kaushik <svkaushik2210@gmail.com>
2026-08-16 00:07:39 +0530

    day 29: add built-in functional interfaces and method reference examples

--- src/day29/LambdasDemo.java
+++ src/day29/LambdasDemo.java
@@ -1,3 +1,8 @@
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+
 public class LambdasDemo {
 
     public static void main(String[] args) {
@@ -19,6 +24,21 @@ public class LambdasDemo {
             }
         };
         System.out.println(viaAnonymousClass.greet("Ravi"));
+
+        Function<String, Integer> length = s -> s.length();
+        Predicate<Integer> isEven = n -> n % 2 == 0;
+        Consumer<String> printer = s -> System.out.println("Got: " + s);
+        Supplier<String> idGenerator = () -> "id-fixed";
+
+        System.out.println(length.apply("hello"));
+        System.out.println(isEven.test(4));
+        printer.accept("done");
+        System.out.println(idGenerator.get());
+
+        Function<String, Integer> lengthRef = String::length; // method reference, same as `length`
+        Consumer<String> printerRef = System.out::println;
+        System.out.println(lengthRef.apply("world"));
+        printerRef.accept("via method reference");
     }
 }
 
