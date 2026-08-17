1dbcc5209150af5090e38bc52d331964fbabd4f0
Kaushik <svkaushik2210@gmail.com>
2026-08-17 07:44:01 +0530

    day 30: add reduce and chained pipeline example

--- src/day30/StreamsDemo.java
+++ src/day30/StreamsDemo.java
@@ -1,4 +1,5 @@
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class StreamsDemo {
@@ -15,5 +16,22 @@ public class StreamsDemo {
             .filter(name -> name.length() > 3)
             .collect(Collectors.toList());
         System.out.println(longNames);
+
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5);
+
+        int sum = numbers.stream().reduce(0, (a, b) -> a + b);
+        System.out.println("sum = " + sum);
+
+        int product = numbers.stream().reduce(1, (a, b) -> a * b);
+        System.out.println("product = " + product);
+
+        Optional<Integer> max = numbers.stream().reduce((a, b) -> a > b ? a : b);
+        System.out.println("max = " + max.get());
+
+        int totalLengthOfLongNames = names.stream()
+            .filter(name -> name.length() > 3) // intermediate
+            .map(String::length)               // intermediate
+            .reduce(0, Integer::sum);           // terminal -- pipeline actually runs here
+        System.out.println("totalLengthOfLongNames = " + totalLengthOfLongNames);
     }
 }
